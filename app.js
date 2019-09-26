e7f6b3922b06ee83a2954c07c11565adc9942928
gundrm <rachelm.gundy@gmail.com>
2019-09-25 20:38:33 -0700

    prepare build for prod

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sendGrid = require('@sendGrid/mail');
+const config = require('config');
+const path = require('path');
 
 const app = express();
 
@@ -47,4 +49,16 @@ app.post('/api/email',(req, res, next) => {
         });
 });
 
-app.listen(3030, '0.0.0.0');
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
